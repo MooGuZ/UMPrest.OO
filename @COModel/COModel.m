1fb2a417d86a168e7e872d831d0a33b0560f0553
MooGuZ <moogu@live.cn>
2015-10-20 10:50:41 -0400

    Added class COModel

---
+++ @COModel/COModel.m
@@ -0,0 +1,18 @@
+% CLASS : COModel
+%
+% Class derived from base class MotionLearner that implementing Cadieu &
+% Olshausen's model in their 2012 paper. This program just implemented the
+% first layer of this two layers model. The first layer is a generative
+% model bases on complex bases. 
+%
+% MooGu Z. <hzhu@case.edn>
+%
+% Oct 20, 2015 - Initial commit
+
+classdef COModel < MotionLearner
+    
+    
+    
+    
+    
+end
