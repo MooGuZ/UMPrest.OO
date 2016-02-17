67b46218dfdf37d23d0745e02f07509dba3090fd
MooGu Z <moogu@live.cn>
2016-02-17 14:01:13 -0500

    added to-do list to 'tools/multimg.m' and fixed indent style

--- tools/multimg.m
+++ tools/multimg.m
@@ -4,21 +4,28 @@ function H = multimg(mat)
 %
 % MooGu Z. <hzhu@case.edu>
 % Feb 14, 2016
-    [imgh, imgw, nimg] = size(mat); % get information of images
-    [nrow, ncol] = arrange(nimg);   % make arrangement of plots
-    % setup figure size
-    scnsz = get(0, 'screensize');
-    ratio = max([ ...
-        1.5 * ncol * imgw / scnsz(3), ...
-        1.5 * nrow * imgh / scnsz(4), ...
-        1]);
-    figw = ncol * imgw / ratio;
-    figh = nrow * imgh / ratio;
-    H = figure('Position', [100, 100, figw, figh]);
-    % display each image
-    for i = 1 : nimg
-        subplot(nrow, ncol, i);
-        imagesc(mat(:, :, i));
-        axis off
-    end
+    
+% [TO-DO]
+% 1. apply subaxis tech to create tight axes
+% 2. add support to other image types (currently only gray images)
+% 3. tweak color render method
+
+[imgh, imgw, nimg] = size(mat); % get information of images
+[nrow, ncol] = arrange(nimg);   % make arrangement of plots
+% setup figure size
+scnsz = get(0, 'screensize');
+ratio = max([ ...
+    1.5 * ncol * imgw / scnsz(3), ...
+    1.5 * nrow * imgh / scnsz(4), ...
+    1]);
+figw = ncol * imgw / ratio;
+figh = nrow * imgh / ratio;
+H = figure('Position', [100, 100, figw, figh]);
+% display each image
+for i = 1 : nimg
+    subplot(nrow, ncol, i);
+    imagesc(mat(:, :, i));
+    axis off
+end
+
 end
