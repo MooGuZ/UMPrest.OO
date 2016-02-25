464b86d1bd0ada19badb9ef44f1a4aece1d838bb
MooGu Z <moogu@live.cn>
2016-02-24 21:50:12 -0500

    added subunit of activation, pooling and norm
    while activation and pooling have been tested, while norm not.

---
+++ subunit/Activation.m
@@ -0,0 +1,72 @@
+classdef Activation < handle
+% ACTIVATION provide functionality of activation function in Neural Network models.
+
+% MooGu Z. <hzhu@case.edu>
+% Feb 23, 2016
+
+    properties (Access = protected)
+        act = struct('type', 'relu', ...
+                     'proc', @Activation.ReLU, ...
+                     'derv', @Activation.ReLU_derv);
+    end
+    
+    properties (Dependent)
+        actType
+    end
+    methods
+        function value = get.actType(obj)
+            value = obj.act.type;
+        end
+        function set.actType(obj, value)
+            switch lower(value)
+              case {'sigmoid', 'logistic'}
+                obj.act.type = value;
+                obj.act.proc = @Activation.sigmoid;
+                obj.act.derv = @Activation.sigmoid_derv;
+              
+              case {'tanh'}
+                obj.act.type = value;
+                obj.act.proc = @Activation.tanh;
+                obj.act.derv = @Activation.tanh_derv;
+                
+              case {'relu'}
+                obj.act.type = value;
+                obj.act.proc = @Activation.ReLU;
+                obj.act.derv = @Activation.ReLU_derv;
+                
+              case {'off'}
+                obj.act.type = 'off';
+                obj.act.proc = @nullfunc;
+                obj.act.derv = @nullfunc;
+                
+              otherwise
+                warning('[ACTIVATION] Unrecognized type');
+            end
+        end
+    end
+    
+    methods (Static)
+        function y = sigmoid(x)
+            y = 1 ./ (1 + exp(-x));
+        end
+        function d = sigmoid_derv(y)
+            d = y .* (1 - y);
+        end
+        
+        function y = hypertgt(x)
+            y = tanh(x);
+        end
+        function d = hypertgt_derv(y)
+            d = 1 - y.^2;
+        end
+        
+        function y = ReLU(x)
+            y = max(x, 0);
+        end
+        function d = ReLU_derv(y)
+            d = ones(size(y));
+            d(y <= 0) = 0;
+        end
+    end
+end
+
