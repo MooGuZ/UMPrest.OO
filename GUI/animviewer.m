61ceaf864807fdd3fca1704a3b7649619f078e42
MooGuZ <moogu@live.cn>
2016-04-29 12:14:42 -0400

    fixed warning about slider in 'animviewer' and 'animcompare'

--- GUI/animviewer.m
+++ GUI/animviewer.m
@@ -1,5 +1,25 @@
 function f = animviewer(data, cmap, resolution)
-% ANIMVIEWER create a GUI object to play given animation with control support
+% ANIMVIEWER create a GUI object to play given animation with support of
+% play/pause button and frame-corresponding slider.
+%
+%   F = ANIMVIEWER(DATA) create a GUI object with figure handle F to play
+%   animation in DATA. Assuming DATA is a 2D or 3D matrix of float values
+%   in range of [0,1] that representing a gray-scale animation. The last
+%   dimension of DATA is considered as axes of animation frames. If DATA is
+%   a 2D matrix, ANIMVIEWER would try to recover frames in the vectors into
+%   NxN matrix automatically. If this operation failed, an error would be
+%   raised.
+%
+%   F = ANIMVIWER(DATA, CMAP) create a GUI object with figure handle F to
+%   play animation in DATA wich color map CMAP, which is a Nx3 matrix
+%   containing RGB color value in each row. DATA here is an index matrix
+%   with 2 or 3 dimension.
+%
+%   F = ANIMVIEWER(DATA, CMAP, RESOLUTION) add capability to recover frames
+%   from vectors with specified resolution in RESOLUTION, which should be a
+%   2 elements vector.
+%
+% See also, animcompare.
 
 % MooGu Z. <hzhu@case.edu>
 % Feb 20, 2016
@@ -58,12 +78,18 @@ function f = animviewer(data, cmap, resolution)
         ws.hanim = imshow(ws.animdata(:, :, 1));
     end
     
+    if (ws.nframe < 10)
+        sliderstep = (1 / ws.nframe) * [1, 1];
+    else
+        sliderstep = [1 / ws.nframe, 0.1];
+    end
+    
     ws.slider = uicontrol('Parent',     f, ...
                           'Style',      'Slider', ...
                           'Value',      1, ...
                           'Max',        ws.nframe, ...
                           'Min',        1, ...
-                          'SliderStep', [1 / ws.nframe, 0.1], ...
+                          'SliderStep', sliderstep, ...
                           'Position',   spos, ...
                           'Callback',   @jumpToFrame);
     
