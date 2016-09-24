61a1e1f4ef17e3d38916cd39b67ac11f29f9b66d
MooGuZ <moogu@live.cn>
2016-09-23 23:22:01 -0400

    remove all legacy files

--- @VideoDatasetOld/traverse.m
+++
@@ -1,8 +0,0 @@
-% This function would modify current underlining status of the
-% object. Normally, it would not effect your work. However, in
-% special application, it maybe a trouble.
-function sample = traverse(obj)
-    obj.restateDataBlock();
-    sample = obj.next(obj.volumn());
-    obj.refreshDataBlock();
-end
--- tools/nullfunc.m
+++
@@ -1,3 +0,0 @@
-function varargout = nullfunc(varargin)
-    varargout = varargin;
-end
