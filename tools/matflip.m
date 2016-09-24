447fa111cfa8cb1f60213c3e39d3810aea7f5441
MooGuZ <moogu@live.cn>
2016-09-23 22:59:16 -0400

    remove all legacy files

--- tools/matflip.m
+++
@@ -1,7 +0,0 @@
-% MATFLIP flip matrix both in vertical and horizontal direction.
-%
-% MooGu Z. <hzhu@case.edu>
-% Feb 13, 2016
-function fmat = matflip(mat)
-fmat = reshape(mat(size(mat, 1) : -1 : 1, size(mat, 2) : -1 : 1, :), size(mat));
-end
--- tools/uwphase.m
+++
@@ -1,9 +0,0 @@
-% UWPHASE unwrap phases in [-pi,pi]
-%
-%   UNWRAP = uwphase(WRAPPED)
-%
-% MooGu Z. <hzhu@case.edu>
-% Nov 30, 2015 - initial commit
-function unwrap = uwphase(wrapped)
-    unwrap = cumsum([wrapped(1), wrapToPi(diff(wrapped(:)'))]);
-end
