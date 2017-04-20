5689ae9829edc9850f2d7fc905a77061a4018f1f
MooGu Z <moogu@live.cn>
2017-04-20 11:10:22 -0400

    update script to run DistModel

--- experiment/NewDistModel.m
+++ experiment/NewDistModel.m
@@ -1,5 +1,8 @@
 taskid  = 'NEWDISTMODEL';
 taskdir = fileparts(mfilename('fullpath'));
+savedir = fullfile(taskdir, 'records');
+istart  = 0;
+namept  = [taskid, '-ITER%d-DUMP.mat'];
 % model parameters
 nfrmin  = 3;
 nfrmout = 3;
@@ -8,12 +11,17 @@ frmsize = [32, 32];
 sizeout = prod([4, 4, nfrmout]);
 ncat    = 5;
 nbasis  = ceil(sizeout * 1.2);
-% create units and model
-cunit   = ConvNet.randinit(nfrmin, [2 * nfrmin, 4 * nfrmin, nfrmout], ...
-    'poolsize', psize, 'OutputLayerActType', 'Sigmoid');
-gunit   = GaussianMixtureUnit(ncat, nbasis, sizeout);
-dshaper = Reshaper().appendto(cunit).aheadof(gunit.apdata);
-model   = Model(cunit, dshaper, gunit);
+% create/load units and model
+if istart == 0
+    cunit   = ConvNet.randinit(nfrmin, [2 * nfrmin, 4 * nfrmin, nfrmout], ...
+        'poolsize', psize, 'OutputLayerActType', 'Sigmoid');
+    gunit   = GaussianMixtureUnit(ncat, nbasis, sizeout);
+    dshaper = Reshaper().appendto(cunit).aheadof(gunit.apdata);
+    model   = Model(cunit, dshaper, gunit);
+else
+    load(fullfile(savedir, sprintf(namept, istart)));
+    model = Evolvable.loaddump(modeldump);
+end
 % create objectives
 prob = ObjSum();
 % load dataset
@@ -26,7 +34,10 @@ nplab3d.label.connect(gunit.aplabel);
 gunit.approb.connect(prob.x);
 % create priors
 distinguish = DistVar(gunit.A);
+distinguish.scale = -1e-2;
+entropy = Entropy(cunit.O{1});
+entropy.scale = -1;
 % create task
-task = CustomTask(taskid, taskdir, model, nplab3d, prob, {distinguish});
+task = CustomTask(taskid, taskdir, model, nplab3d, prob, {distinguish, entropy});
 % run task
-task.run(10, 100, 16, 64);
+task.run(10, 10, 64, 64);
