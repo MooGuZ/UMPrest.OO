9e8efd05df9dd3c41e382fa5aa51f7a48f5534da
MooGuZ <moogu@live.cn>
2017-09-28 11:25:14 -0400

    add class 'ProbabilityDescription' to manage priors

---
+++ ProbabilityDescription.m
@@ -0,0 +1,41 @@
+classdef ProbabilityDescription < handle
+    methods
+        function addPrior(self, varargin)
+            self.priorSet = [self.priorSet, {CommonPrior(varargin{:})}];
+        end
+        
+        function clearPrior(self)
+            self.priorSet = {};
+        end
+        
+        function priorDisplay(self)
+            fprintf('[Prior List]\n\n');
+            for i = 1 : numel(self.priorSet)
+                disp(self.priorSet{i});
+                fprintf('\n\n');
+            end
+        end
+        
+        function value = priorEval(self, data)
+            value = 0;
+            for i = 1 : numel(self.priorSet)
+                value = value + self.priorSet{i}.evaluate(data);
+            end
+        end
+        
+        function d = priorDelta(self, data)
+            if isempty(self.priorSet)
+                d = 0;
+            else                
+                d = self.priorSet{1}.delta(data);
+                for i = 2 : numel(self.priorSet)
+                    d = d + self.priorSet{i}.delta(data);
+                end
+            end
+        end
+    end
+    
+    properties (SetAccess = protected)
+        priorSet = {}
+    end        
+end
