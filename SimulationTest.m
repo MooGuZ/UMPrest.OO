b6f8745d4a8b18d41e708d2e02f07b25be4c7155
MooGu Z <moogu@live.cn>
2017-10-01 23:28:58 -0400

    update 'SimulationTest' to support multiple output unit's test
    Currently, it identifies multiple output by quantity of objectives
    assigned to it. Obviously, this is not a good way. However, because of
    state input/output existence checking I/O size isn’t a good solution
    either. This problem related to the fundamental mechanism of IO, need
    more consideration.

--- SimulationTest.m
+++ SimulationTest.m
@@ -6,11 +6,25 @@ classdef SimulationTest < Task
             else
                 validset.data = {obj.dataset.next(validsize)};
             end
-            validset.label = obj.ref.forward(validset.data{:});
+            if iscell(obj.objective)
+                validset.label = cell(1, numel(obj.objective));
+                [validset.label{:}] = obj.ref.forward(validset.data{:});
+            else                
+                validset.label = obj.ref.forward(validset.data{:});
+            end
             % get optimizer
             opt = HyperParam.getOptimizer();
             % check objective value of current state
-            objval = obj.objective.evaluate(obj.model.forward(validset.data{:}), validset.label);
+            if iscell(obj.objective)
+                objval = 0;
+                output = cell(1, numel(obj.objective));
+                [output{:}] = obj.model.forward(validset.data{:});
+                for i = 1 : numel(obj.objective)
+                    objval = objval + obj.objective{i}.evaluate(output{i}, validset.label{i});
+                end
+            else                
+                objval = obj.objective.evaluate(obj.model.forward(validset.data{:}), validset.label);
+            end
             distinfo(abs(obj.ref.dumpraw() - obj.model.dumpraw()), 'HPARAM ERROR', false);
             disp(repmat('=', 1, 100));
             fprintf('Initial objective value : %.2e\n', objval);
@@ -24,11 +38,33 @@ classdef SimulationTest < Task
                 else
                     data = {obj.dataset.next(batchsize)};
                 end
-                label = obj.ref.forward(data{:});
-                odata = obj.model.forward(data{:});
-                obj.model.backward(obj.objective.delta(odata, label));
+                if iscell(obj.objective)
+                    label = cell(1, numel(obj.objective));
+                    odata = cell(1, numel(label));
+                    delta = cell(1, numel(label));
+                    [label{:}] = obj.ref.forward(data{:});
+                    [odata{:}] = obj.model.forward(data{:});
+                    for j = 1 : numel(delta)
+                        delta{j} = obj.objective{j}.delta(odata{j}, label{j});
+                    end
+                    obj.model.backward(delta{:});
+                else
+                    label = obj.ref.forward(data{:});
+                    odata = obj.model.forward(data{:});
+                    obj.model.backward(obj.objective.delta(odata, label));
+                end                
                 obj.model.update();
-                objval = obj.objective.evaluate(obj.model.forward(validset.data{:}), validset.label);
+                % check objective value of current state
+                if iscell(obj.objective)
+                    objval = 0;
+                    output = cell(1, numel(obj.objective));
+                    [output{:}] = obj.model.forward(validset.data{:});
+                    for j = 1 : numel(obj.objective)
+                        objval = objval + obj.objective{j}.evaluate(output{j}, validset.label{j});
+                    end
+                else                
+                    objval = obj.objective.evaluate(obj.model.forward(validset.data{:}), validset.label);
+                end
                 fprintf('Objective Value after [%04d] turns: %.2e\n', i, objval);
                 if mod(i, 10) && opt.rcdmode.status
                     opt.record(objval, false);
