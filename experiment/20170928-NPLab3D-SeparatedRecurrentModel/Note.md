a99a5b5501918960829d3e9106969c132e6a3313
MooGuZ <moogu@live.cn>
2017-10-02 18:09:11 -0400

    update script of experiment and analysis, and note

--- experiment/20170928-NPLab3D-SeparatedRecurrentModel/Note.md
+++ experiment/20170928-NPLab3D-SeparatedRecurrentModel/Note.md
@@ -3,3 +3,9 @@
 ![material/DesignPlot.png](Network Design)
 
 This experiment tries to test PHLSTM-Codec's power on 3D motion dataset. Before it, I have tried original PHLSTM-Codec on NPLab3D dataset, and obtained frame-mean as prediction. However, even earlier, I tested *RecCO* model on NPLab3D and obtained predictions that can get a sense of motion (very weak though). This time, I would like to combine them together. This design is essential a codec for *RecCO* unit.
+
+Mon Oct  2 14:39:10 EDT 2017
+After about 60 hours training, model run 70k iterations (batchsize = 32). The result I get can reconstruct a better result than *RecCO* model. It can correctly predict the transformation of objects (opposite to background). So in scaling and rotation with object away from center I can get a good sense of motion in the model prediction of 15 frames based on 15 frames input. However, the reconstruction of background (most of time is the ground in 3D scenes) is quite noisy and cannot preserve its motion pattern. This is easy to understand. Because the texture on the ground is very weak and can not make big difference in terms of MSE in reconstruction. So, the objective function I used in training pretend to make trade-off on it. Besides, there is underlying overfitting problem in this model. Currently, effective dimension (after whitening) of a frame is 269, while only 20k sequence used in training, and I don't make separation of training and testing set here.
+
+Mon Oct  2 18:06:00 EDT 2017
+Use Dual-PHLSTM (DPHLSTM) to continue the optimization process on NPLab3D dataset.
--- experiment/20170928-NPLab3D-SeparatedRecurrentModel/dphlstm1024nplab3d.m
+++ experiment/20170928-NPLab3D-SeparatedRecurrentModel/dphlstm1024nplab3d.m
@@ -1,5 +1,5 @@
 % MODEL : Separated Recurrent Model on NPLab3D dataset
-% CODE  : https://github.com/MooGuZ/UMPrest.OO/commit/ed6f503953393fe8a17cb3ac6ecd6d79f59944c6
+% CODE  : 
 %% check environment
 ishpc = isunix && not(ismac);
 %% load package to MATLAB search path
@@ -17,10 +17,10 @@ if ishpc
     validsize = 128;
     taskdir   = fileparts(mfilename('fullpath'));
 else
-    nhidunit  = 64;
+    nhidunit  = 1024;
     nloop     = 3;
     nepoch    = 3;
-    nbatch    = 7;
+    nbatch    = 3;
     batchsize = 8;
     validsize = 32;
     taskdir   = pwd();
@@ -31,7 +31,7 @@ nframePredict = 15;
 initEstch = 1e-3;
 %% environment parameters
 istart  = 0;
-taskid  = ['SRM', num2str(nhidunit), 'NPLAB3D'];
+taskid  = ['DPHLSTM', num2str(nhidunit), 'NPLAB3D'];
 savedir = fullfile(taskdir, 'records');
 datadir = fullfile(taskdir, 'data');
 namept  = [taskid, '-ITER%d-DUMP.mat'];
@@ -45,36 +45,31 @@ comodel = load(fullfile(datadir, 'comodel_nplab3d.mat'));
 whitening = StatisticTransform('whiten', nplab3d.stat).appendto(nplab3d.data);
 stat      = whitening.getKernel(framesize);
 %% create/load units and model
-if istart == 0
-    ampEncoder  = PHLSTM.randinit(nhidunit);
-    ampPredict  = PHLSTM.randinit(nhidunit, [], nbases);
-    angEncoder  = PHLSTM.randinit(nhidunit);
-    angPredict  = PHLSTM.randinit(nhidunit, [], nbases);
-    reTransform = LinearTransform.randinit(stat.sizeout, nhidunit);
-    imTransform = LinearTransform.randinit(stat.sizeout, nhidunit);
-else
+% if istart == 0
+%     encoder     = DPHLSTM.randinit(nhidunit, [], [], [], []);
+%     predict     = DPHLSTM.randinit(nhidunit, [], nbase, [], nbase);
+%     reTransform = LinearTransform.randinit(stat.sizeout, nhidunit);
+%     imTransform = LinearTransform.randinit(stat.sizeout, nhidunit);
+% else
     load(fullfile(savedir, sprintf(namept, istart)));
-    ampEncoder  = Interface.loaddump(ampencoderdump);
-    ampPredict  = Interface.loaddump(amppredictdump);
-    angEncoder  = Interface.loaddump(angencoderdump);
-    angPredict  = Interface.loaddump(angpredictdump);
+    encoder     = Interface.loaddump(encoderdump);
+    predict     = Interface.loaddump(predictdump);
     reTransform = Interface.loaddump(retransformdump);
     imTransform = Interface.loaddump(imtransformdump);
-end
-ampEncoder.stateAheadof(ampPredict);
-angEncoder.stateAheadof(angPredict);
+% end
+encoder.stateAheadof(predict);
 % create assistant units
 crdTransform = Cart2Polar().appendto( ...
-    reTransform, imTransform).aheadof(ampEncoder.DI{1}, angEncoder.DI{1});
-model = Model(reTransform, imTransform, crdTransform, ampEncoder, ampPredict, angEncoder, angPredict);
+    reTransform, imTransform).aheadof(encoder.DI{1}, encoder.DI{2});
+model = Model(reTransform, imTransform, crdTransform, encoder, predict);
 %% create prevnet
 inputSlicer  = FrameSlicer(nframeEncoder, 'front', 0).appendto( ...
     whitening).aheadof(reTransform).aheadof(imTransform);
 outputSlicer = FrameSlicer(nframePredict, 'front', nframeEncoder).appendto(whitening);
 prevnet = Model(whitening, inputSlicer, outputSlicer);
 %% create postnet
-ampact = SimpleActivation('ReLU').appendto(ampPredict.DO{1});
-angact = SimpleActivation('tanh').appendto(angPredict.DO{1});
+ampact = SimpleActivation('ReLU').appendto(predict.DO{1});
+angact = SimpleActivation('tanh').appendto(predict.DO{2});
 angscaler = Scaler(pi).appendto(angact);
 cotransform = PolarCLT(comodel.rweight, comodel.iweight, zeros(stat.sizeout, 1)).appendto( ...
     ampact, angscaler);
@@ -82,11 +77,11 @@ cotransform.freeze();
 postnet = Model(ampact, angact, angscaler, cotransform);
 %% create zero generators
 zerogen = DataGenerator('zero', nhidunit, 'tmode', nframeEncoder);
-zerogen.data.connect(ampPredict.DI{1});
-zerogen.data.connect(angPredict.DI{1});
+zerogen.data.connect(predict.DI{1});
+zerogen.data.connect(predict.DI{2});
 errgen = DataGenerator('zero', nhidunit, 'tmode', nframeEncoder, '-errmode');
-errgen.data.connect(ampEncoder.DO{1});
-errgen.data.connect(angEncoder.DO{1});
+errgen.data.connect(encoder.DO{1});
+errgen.data.connect(encoder.DO{2});
 %% create objectives
 lossfun = Likelihood('mse', stat.pixelweight);
 lossfun.x.connect(cotransform.O{1});
@@ -104,15 +99,12 @@ opt.enableRcdmode(3);
 latestsave = [];
 for i = 1 : nloop
     task.run(nepoch, nbatch, batchsize, validsize);
-    ampencoderdump  = ampEncoder.dump();
-    amppredictdump  = ampPredict.dump();
-    angencoderdump  = angEncoder.dump();
-    angpredictdump  = angPredict.dump();
+    encoderdump     = encoder.dump();
+    predictdump     = predict.dump();
     retransformdump = reTransform.dump();
     imtransformdump = imTransform.dump();
     save(fullfile(savedir, sprintf(namept, task.iteration)), ...
-        'ampencoderdump', 'amppredictdump', 'retransformdump', ...
-        'angencoderdump', 'angpredictdump', 'imtransformdump', '-v7.3');
+        'encoderdump', 'predictdump', 'retransformdump', 'imtransformdump', '-v7.3');
     if not(isempty(latestsave))
         delete(latestsave);
     end
