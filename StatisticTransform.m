9b1b169122867812423e60cdbc6ac361e806eedf
MooGu Z <moogu@live.cn>
2017-10-10 23:20:29 -0400

    add support to specify output size in whitening mode

--- StatisticTransform.m
+++ StatisticTransform.m
@@ -1,7 +1,7 @@
 classdef StatisticTransform < SISOUnit & BidirectionOperation
     methods    
         function data = dataproc(obj, data)
-            if not(obj.outsourced)
+            if not(obj.outsourced) && not(obj.frozen)
                 obj.stat.commit(data);
             end
             
@@ -67,7 +67,7 @@ classdef StatisticTransform < SISOUnit & BidirectionOperation
         end
         
         function error = deltainvp(obj, error)
-            kernel = obj.getKernel();
+            kernel = obj.getKernel(size(error));
             switch obj.mode
                 case {'debias'}
                     % DO NOTHING
@@ -115,16 +115,68 @@ classdef StatisticTransform < SISOUnit & BidirectionOperation
     end
     
     methods
+        function refreshCache(obj)            
+            obj.cache = containers.Map('KeyType', 'char', 'ValueType', 'any');
+            obj.cache('timestamp') = obj.stat.count;            
+        end
+                
         function tof = cacheOutdated(obj)
             if isempty(obj.cache)
                 obj.refreshCache();
-            end
-            
-            if obj.frozen
                 tof = false;
             else
                 tof = (obj.stat.count - obj.cache('timestamp') >= obj.updateInterval);
+            end            
+        end
+        
+        function kernel = updateCache(obj, inputSize)
+            sinfo = obj.stat.fetch(inputSize);
+            
+            kernel.sizein = inputSize;
+            kernel.offset = sinfo.mean;
+            kernel.scale  = sinfo.std;
+            
+            if strcmpi(obj.mode, 'whiten')
+                [vec, val] = eig(sinfo.covmat);
+                [val, idx] = sort(diag(val), 'descend');
+                vec = vec(:, idx);
+                % >>> Cadieu & Olshausen's method <<<
+                % threshold = pixelvar * obj.whitenCutoffRatio;
+                % kernel.sizeout = sum(val > threshold);
+                % >>> Method based on percentage of Power <<<
+                % - practise shows '0.95' is good point to choose, which is
+                % - balance for performance and storage sufficience.
+                % cumval = cumsum(val);
+                % kernel.sizeout = sum((cumval / cumval(end)) < obj.whitenCutoffRatio);
+                % >>> Method use quantity ratio <<<
+                % kernel.sizeout = ceil(prod(kernel.sizein(1 : obj.dsample)) ...
+                %     * obj.whitenCompressRatio);
+                % >>> get output dimension by setting <<<
+                kernel.sizeout = numel(sinfo.sum);
+                if not(isempty(obj.whitenSizeOut))
+                    kernel.sizeout = min(obj.whitenSizeOut, kernel.sizeout);
+                    val = val(1 : kernel.sizeout);
+                    vec = vec(:, 1 : kernel.sizeout);                                    
+                end
+                % calculate pixel weight
+                % >>> Cadieu & Olshausen's method <<<
+                % npixel = sinfo.count * numel(sinfo.sum);
+                % pixelvar = (sum(sinfo.sum2(:)) / npixel) ...
+                %     - (sum(sinfo.sum(:)) / npixel).^2;
+                % rodim = sum(val > pixelvar * obj.whitenRolloffFactor);
+                % kernel.pixelweight = ...
+                %     MathLib.rolloff(dim, rodim) / obj.whitenNoiseRatio;
+                % >>> use eigen value <<<
+                kernel.pixelweight = sqrt(val);
+                % transformation matrix                
+                kernel.encode    = diag(1 ./ sqrt(val)) * vec';
+                kernel.decode    = vec * diag(sqrt(val));
+                kernel.zerophase = vec * diag(1 ./ sqrt(val)) * vec';
+            else
+                kernel.sizeout     = numel(sinfo.sum);
+                kernel.pixelweight = ones(size(sinfo.sum), 'like', sinfo.sum);
             end
+            obj.cache(mat2str(inputSize)) = kernel;
         end
         
         function kernel = getKernel(obj, inputSize)
@@ -148,55 +200,23 @@ classdef StatisticTransform < SISOUnit & BidirectionOperation
             end
             
             obj.lastSampleSize = inputSize;
-        end
-        
-        function refreshCache(obj)
-            obj.cache = containers.Map('KeyType', 'char', 'ValueType', 'any');
-            obj.cache('timestamp') = obj.stat.count;
-        end
-        
-        function kernel = updateCache(obj, inputSize)
-            sinfo = obj.stat.fetch(inputSize);
-            
-            kernel.sizein = inputSize;
-            kernel.offset = sinfo.mean;
-            kernel.scale  = sinfo.std;
-            
-            [vec, val] = eig(sinfo.covmat);
-            [val, idx] = sort(diag(val), 'descend');
-            vec = vec(:, idx);
-            npixel = sinfo.count * numel(sinfo.sum);
-            pixelvar = (sum(sinfo.sum2(:)) / npixel) ...
-                - (sum(sinfo.sum(:)) / npixel).^2;
-            % >>> Cadieu & Olshausen's method <<<
-            % threshold = pixelvar * obj.whitenCutoffRatio;
-            % kernel.sizeout = sum(val > threshold);
-            % >>> Method based on percentage of Power <<<
-            % - practise shows '0.95' is good point to choose, which is
-            % - balance for performance and storage sufficience.
-            cumval = cumsum(val);
-            kernel.sizeout = sum((cumval / cumval(end)) < obj.whitenCutoffRatio);
-            % >>> Method use quantity ratio
-            % kernel.sizeout = ceil(prod(kernel.sizein(1 : obj.dsample)) ...
-            %     * obj.whitenCompressRatio);
-            dim = kernel.sizeout;
-            rodim = sum(val > pixelvar * obj.whitenRolloffFactor);
-            kernel.pixelweight = ...
-                MathLib.rolloff(dim, rodim) / obj.whitenNoiseRatio;
-            val = val(1 : dim);
-            vec = vec(:, 1 : dim);
-            
-            kernel.encode    = diag(1 ./ sqrt(val)) * vec';
-            kernel.decode    = vec * diag(sqrt(val));
-            kernel.zerophase = vec * diag(1 ./ sqrt(val)) * vec';
-            
-            obj.cache(mat2str(inputSize)) = kernel;
+        end        
+    end
+    
+    methods
+        function obj = compressOutput(obj, n)
+            if strcmpi(obj.mode, 'whiten') && n > 0
+                obj.whitenSizeOut = n;
+            else
+                obj.whitenSizeOut = [];
+            end
         end
     end
     
     methods
-        function obj = StatisticTransform(mode, arg, varargin)
-            obj.mode = mode;
+        function obj = StatisticTransform(arg, varargin)
+            conf = Config(varargin);
+            % initialize statistic collector
             if isa(arg, 'StatisticCollector')
                 obj.stat = arg;
                 obj.outsourced = true;
@@ -207,6 +227,11 @@ classdef StatisticTransform < SISOUnit & BidirectionOperation
                 obj.stat = StatisticCollector(arg);
                 obj.outsourced = false;
             end
+            % setup mode is exist
+            if conf.exist('mode')
+                obj.mode = conf.pop('mode');
+            end
+            % setup access points
             obj.I = {UnitAP(obj, obj.dsample)};
             switch obj.mode
                 case {'debias', 'normalize'}
@@ -215,20 +240,34 @@ classdef StatisticTransform < SISOUnit & BidirectionOperation
                 case {'whiten'}
                     obj.O = {UnitAP(obj, 1)};
             end
-            obj = Config(varargin).apply(obj);
+            % apply other arguments
+            obj = conf.apply(obj);
         end
         
         function unitdump = dump(self)
-            unitdump = {'StatisticTransform', self.mode, self.stat};
+            unitdump = {'StatisticTransform', self.stat, 'mode', self.mode};
         end
     end
     
+    properties (Constant)
+        taxis = false;
+    end
     properties
-        whitenCutoffRatio   = 0.99;
-        whitenCompressRatio = 0.5; % TBC in parameter setup
-        whitenRolloffFactor = 8;
-        whitenNoiseRatio    = 0.01;
-        updateInterval      = 1000;
+        % whitenCutoffRatio   = 0.99;
+        % whitenCompressRatio = 0.5; % TBC in parameter setup
+        % whitenRolloffFactor = 8;
+        % whitenNoiseRatio    = 0.01;
+        updateInterval = 1000
+        frozen = false
+    end
+    properties (SetAccess = protected)
+        mode = 'whiten', lastSampleSize, whitenSizeOut
+    end
+    properties (SetAccess = private, Hidden)
+        stat, cache, outsourced
+    end
+    properties (Dependent)
+        dsample
     end
     methods
         function set.updateInterval(obj, value)
@@ -236,39 +275,19 @@ classdef StatisticTransform < SISOUnit & BidirectionOperation
                 'Interval of update should be a positive integer.');
             obj.updateInterval = value;
         end
-    end
-    
-    properties (Constant)
-        taxis      = false;
-        expandable = true;
-    end
-    
-    properties
-        mode, frozen, lastSampleSize
-    end
-    methods
+        
         function set.mode(obj, value)
             validatestring(lower(value), {'debias', 'normalize', 'whiten'});
             obj.mode = lower(value);
         end
-    end
-    
-    properties (Dependent)
-        dsample
-    end
-    methods
-        function value = get.dsample(obj)
-            value = obj.stat.dsample;
-        end
-    end
-    
-    properties (SetAccess = private, Hidden)
-        stat, cache, outsourced
-    end
-    methods
+        
         function set.stat(obj, value)
             assert(isa(value, 'StatisticCollector'));
             obj.stat = value;
+        end
+        
+        function value = get.dsample(obj)
+            value = obj.stat.dsample;
         end
     end
 end
