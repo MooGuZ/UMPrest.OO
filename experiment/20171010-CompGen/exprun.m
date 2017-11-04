6b4d8ef982c93e59e3a37162a6b40fcc38924b7c
MooGu Z <moogu@live.cn>
2017-11-04 09:48:04 -0400

    recalibrate the objective functions in complex generative model's experiment

--- experiment/20171010-CompGen/exprun.m
+++ experiment/20171010-CompGen/exprun.m
@@ -10,13 +10,13 @@ end
 %% model parameters
 if ishpc
     nhidunit  = 1024;
-    nepoch    = 100;
+    nepoch    = 40;
     nbatch    = 500;
     batchsize = 32;
     validsize = 128;
     taskdir   = fileparts(mfilename('fullpath'));
 else
-    nhidunit  = 64;
+    nhidunit  = 1024;
     nepoch    = 3;
     nbatch    = 3;
     batchsize = 8;
@@ -26,16 +26,18 @@ end
 whitenSizeOut = 512;
 initEstch = 1e-2;
 %% environment parameters
-istart  = 0;
+istart  = 5e5;
 taskid  = ['COMPGEN', num2str(nhidunit), 'TRANSFORM2D'];
 savedir = fullfile(taskdir, 'records');
 datadir = fullfile(taskdir, 'data');
 namept  = [taskid, '-ITER%d-DUMP.mat'];
 %% load dataset and parameter setup
 dataset = Transform2D();
+nframes = dataset.nframes;
 framesize = dataset.framesize;
 %% load statistic information
 load(fullfile(datadir, 'statrans_transform2d.mat'));
+% stunit = Interface.loaddump(stdump);
 stunit = Interface.loaddump(stdump);
 stunit.frozen = true;
 stunit.compressOutput(whitenSizeOut);
@@ -45,16 +47,19 @@ if istart == 0
     model = GenerativeUnit(PolarCLT.randinit(nhidunit, whitenSizeOut));
 else
     load(fullfile(savedir, sprintf(namept, istart)));
+    % model = Interface.loaddump(modeldump);
     model = Interface.loaddump(modeldump);
 end
-model.O{1}.addPrior('cauchy', 'scale', 10, 'stdvar', 0.4);
-model.O{1}.addPrior('slow', 'stdvar', sqrt(2));
 model.kernel.useCOModelNormalization = true;
+model.noiseStdvar = 0.3;
+model.I{1}.objweight = sqrt(stat.pixelweight);
+sparse = model.O{1}.addPrior('cauchy', 'stdvar', sqrt(2));
+slow   = model.O{1}.addPrior('slow', 'stdvar', sqrt(2));
 %% create prevnet
 stunit.appendto(dataset.data).aheadof(model);
 prevnet = stunit;
 %% create objectives
-lossfun = Likelihood('mse', stat.pixelweight * sqrt(whitenSizeOut * validsize));
+lossfun = Likelihood('mse', sqrt(stat.pixelweight * whitenSizeOut * nframes) / model.noiseStdvar);
 lossfun.x.connect(model.I{1});
 lossfun.ref.connect(stunit.O{1});
 %% create task
