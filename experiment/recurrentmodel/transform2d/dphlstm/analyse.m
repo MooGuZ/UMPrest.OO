109f3bcee1a6a5e2b759344644832c7f8f28ddc1
MooGuZ <moogu@live.cn>
2017-11-07 18:38:25 -0500

    update analyze script for DPHLSTM model

--- experiment/recurrentmodel/transform2d/dphlstm/analyse.m
+++ experiment/recurrentmodel/transform2d/dphlstm/analyse.m
@@ -3,25 +3,26 @@
 %% model parameters
 nbases   = 1024;
 nhidunit = 1024;
-nwhiten  = 269;
+nwhiten  = 512;
 nframeEncoder = 15;
 nframePredict = 15;
 %% enviroment variables
-istart  = 5.5e4;
-taskid  = ['DPHLSTM', num2str(nhidunit), 'NPLAB3D'];
+istart  = 5e4;
+taskid  = ['DPHLSTM', num2str(nhidunit), 'TRANSFORM2D'];
 taskdir = exproot();
 savedir = fullfile(taskdir, 'records');
 datadir = fullfile(taskdir, 'data');
 plotdir = fullfile(taskdir, 'fig');
 namept  = [taskid, '-ITER%d-DUMP.mat'];
 %% load dataset and parameter setup
-load(fullfile(datadir, 'nplab3d.mat'));
-framesize = nplab3d.stat.smpsize;
+dataset   = Transform2D('nobjects', 1, 'nframes', nframeEncoder + nframePredict);
+framesize = dataset.framesize;
 npixel    = prod(framesize);
-%% create whitening module
-stunit = StatisticTransform(nplab3d.stat, 'mode', 'whiten');
+%% create stunit module
+load(fullfile(datadir, 'statrans_transform2d.mat'));
+stunit = Interface.loaddump(stdump);
 stunit.compressOutput(nwhiten);
-stunit.appendto(nplab3d.data);
+stunit.frozen = true;
 stat = stunit.getKernel(framesize);
 %% load model
 load(fullfile(savedir, sprintf(namept, istart)));
@@ -38,20 +39,22 @@ crdTransform = Cart2Polar().appendto( ...
 model = Model(reTransform, imTransform, crdTransform, encoder, predict);
 %% create prevnet
 inputSlicer  = FrameSlicer(nframeEncoder, 'front', 0).appendto( ...
-    stunit).aheadof(reTransform).aheadof(imTransform);
-outputSlicer = FrameSlicer(nframePredict, 'front', nframeEncoder).appendto(stunit);
+    dataset.data).aheadof(stunit);
+outputSlicer = FrameSlicer(nframePredict, 'front', nframeEncoder).appendto(dataset.data);
+% connect stunit
+stunit.aheadof(reTransform).aheadof(imTransform);
+% compose prevnet
 prevnet = Model(stunit, inputSlicer, outputSlicer);
 %% load COModel bases
-comodel = load(fullfile(datadir, 'comodel_nplab3d.mat'));
+comodel = load(fullfile(datadir, 'comodel_transform2d.mat'));
 %% create postnet
 ampact = SimpleActivation('ReLU').appendto(predict.DO{1});
 angact = SimpleActivation('tanh').appendto(predict.DO{2});
 angscaler = Scaler(pi).appendto(angact);
 cotransform = PolarCLT(comodel.rweight, comodel.iweight, zeros(stat.sizeout, 1)).appendto( ...
     ampact, angscaler);
-recompModel = LinearTransform(stat.decode, stat.offset(:)).appendto(cotransform);
-recompRefer = LinearTransform(stat.decode, stat.offset(:)).appendto(outputSlicer);
-postnet = Model(ampact, angact, angscaler, cotransform, recompModel, recompRefer);
+dewhiten = LinearTransform(stat.decode, stat.offset(:)).appendto(cotransform);
+postnet = Model(ampact, angact, angscaler, cotransform, dewhiten);
 %% create zero generators
 zerogen = DataGenerator('zero', nhidunit, 'tmode', nframeEncoder);
 zerogen.data.connect(predict.DI{1});
@@ -60,16 +63,16 @@ errgen = DataGenerator('zero', nhidunit, 'tmode', nframeEncoder, '-errmode');
 errgen.data.connect(encoder.DO{1});
 errgen.data.connect(encoder.DO{2});
 %% get test sample
-nplab3d.next(8);
+dataset.next(8);
 zerogen.next(8);
 prevnet.forward();
 model.forward();
 postnet.forward();
 %% show results
-animorg  = nplab3d.data.packagercd;
-animref  = recompRefer.O{1}.packagercd.reshape(framesize);
-animpred = recompModel.O{1}.packagercd.reshape(framesize);
-animview({animref, animpred});
+animorg  = inputSlicer.O{1}.packagercd;
+animref  = outputSlicer.O{1}.packagercd.reshape(framesize);
+animpred = dewhiten.O{1}.packagercd.reshape(framesize);
+animview({animorg, animref, animpred});
 %% save animations
 % save(fullfile(plotdir, [taskid, '-ITER', num2str(istart), '-Sample.mat']), ...
 %     'animorg', 'animref', 'animpred', '-v7.3');
