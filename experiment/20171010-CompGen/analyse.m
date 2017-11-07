b6627ddba11799a6b05de7ade4a49eedd084d59c
MooGu Z <moogu@live.cn>
2017-11-07 01:12:52 -0500

    update experiment script of complex-bases generative model
    Also apply a new scheme that use function ‘exproot’, which is not
    included in the repository, to locate experiment folder on different
    computer. In this way, there is no need to copy experiment script to
    the folder and copy back to update the modifications.

--- experiment/20171010-CompGen/analyse.m
+++ experiment/20171010-CompGen/analyse.m
@@ -1,13 +1,13 @@
-% MODEL : Separated Recurrent Model on NPLab3D dataset
-% CODE  : 
+% MODEL : Complex Generative Model on Transform2D dataset
+% CODE  : https://github.com/MooGuZ/UMPrest.OO/commit/c5ed9b6fc7cfe750ad85cea4068bfd74249ec5b8
 %% model parameters
 nhidunit = 1024;
 validsize = 8;
 whitenSizeOut = 512;
 %% enviroment variables
-istart  = 5e5;
+istart  = 5e4;
 taskid  = ['COMPGEN', num2str(nhidunit), 'TRANSFORM2D'];
-taskdir = pwd();
+taskdir = exproot();
 savedir = fullfile(taskdir, 'records');
 datadir = fullfile(taskdir, 'data');
 plotdir = fullfile(taskdir, 'fig');
@@ -18,45 +18,36 @@ framesize = dataset.framesize;
 %% load statistic information
 load(fullfile(datadir, 'statrans_transform2d.mat'));
 stunit = Interface.loaddump(stdump);
-stunit.frozen = true;
 stunit.compressOutput(whitenSizeOut);
+stunit.frozen = true;
+% connect to dataset
+stunit.appendto(dataset.data);
+% get statistic information
 stat = stunit.getKernel(framesize);
 %% load units and model
 load(fullfile(savedir, sprintf(namept, istart)));
 model = Interface.loaddump(modeldump);
 model.I{1}.objweight = stat.pixelweight;
-sparse = model.O{1}.addPrior('cauchy', 'scale', 10, 'stdvar', 0.4);
-slow   = model.O{1}.addPrior('slow', 'stdvar', sqrt(2));
-% model.kernel.useCOModelNormalization = true;
-model.frozen = true;
+model.O{1}.addPrior('cauchy', 'stdvar', sqrt(2));
+model.O{1}.addPrior('slow',   'stdvar', sqrt(2));
+model.noiseStdvar = 0.3;
+% connect to whitening unit
+model.appendto(stunit);
 %% setup inference option
 model.inferOption = struct( ...
-    'Method',      'bb',  ...
+    'Method',      'lbfgs',  ...
     'Display',     'iter', ...
-    'MaxIter',     30,    ...
-    'MaxFunEvals', 1e4);
-%% create prevnet
-stunit.appendto(dataset.data).aheadof(model);
-prevnet = stunit;
-%% create objectives
-lossfun = Likelihood('mse', stat.pixelweight * sqrt(whitenSizeOut * validsize));
-lossfun.x.connect(model.I{1});
-lossfun.ref.connect(stunit.O{1});
+    'MaxIter',     40,    ...
+    'MaxFunEvals', 50);
 %% reconstruction process
-% dpkg = dataset.next(validsize);
-wpkg = prevnet.forward(dpkg);
+dpkg = dataset.next(validsize);
+wpkg = stunit.forward(dpkg);
 [alpha, phi] = model.forward(wpkg);
 rwpkg = model.backward(alpha, phi);
-rpkg  = prevnet.backward(rwpkg);
+rpkg  = stunit.backward(rwpkg);
 %% calculate objectives
-likelihood = lossfun.evaluate(rwpkg, wpkg);
-sparsePrior = sparse.evaluate(model.O{1}.data);
-slowPrior   = slow.evaluate(model.O{1}.data);
-fprintf('[Reconstruction] Likelihood : %.2e | Sparse : %.2e | Slow : %.2e\n', ...
-    likelihood, sparsePrior, slowPrior);
+[likelihood, prior] = model.status();
+fprintf('Objective Value :  %.5e <L:%.2e| P:%.2e>\n', likelihood + prior, likelihood, prior);
 %% show animation
 animview({dpkg, rpkg});
-%% save animations
-% save(fullfile(plotdir, [taskid, '-ITER', num2str(istart), '-Sample.mat']), ...
-%     'animorg', 'animref', 'animpred', '-v7.3');
-
+%% END
--- experiment/20171010-CompGen/exprun.m
+++ experiment/20171010-CompGen/exprun.m
@@ -1,5 +1,5 @@
 % MODEL : Complex Generative Model on Transform2D dataset
-% CODE  : 
+% CODE  : https://github.com/MooGuZ/UMPrest.OO/commit/c5ed9b6fc7cfe750ad85cea4068bfd74249ec5b8
 %% check environment
 ishpc = isunix && not(ismac);
 %% load package to MATLAB search path
@@ -15,13 +15,15 @@ if ishpc
     batchsize = 32;
     validsize = 128;
     taskdir   = fileparts(mfilename('fullpath'));
+    taskopt   = {};
 else
     nhidunit  = 1024;
     nepoch    = 3;
     nbatch    = 3;
     batchsize = 8;
     validsize = 32;
-    taskdir   = pwd();
+    taskdir   = exproot();
+    taskopt   = {'-nosave'};
 end
 whitenSizeOut = 512;
 initEstch = 1e-3;
@@ -37,40 +39,33 @@ nframes = dataset.nframes;
 framesize = dataset.framesize;
 %% load statistic information
 load(fullfile(datadir, 'statrans_transform2d.mat'));
-% stunit = Interface.loaddump(stdump);
 stunit = Interface.loaddump(stdump);
 stunit.frozen = true;
 stunit.compressOutput(whitenSizeOut);
+stunit.appendto(dataset.data);
 stat = stunit.getKernel(framesize);
 %% create/load units and model
 if istart == 0
     model = GenerativeUnit(PolarCLT.randinit(nhidunit, whitenSizeOut));
 else
     load(fullfile(savedir, sprintf(namept, istart)));
-    % model = Interface.loaddump(modeldump);
     model = Interface.loaddump(modeldump);
 end
+model.appendto(stunit);
 model.kernel.useCOModelNormalization = true;
-model.noiseStdvar = 0.3;
 model.I{1}.objweight = sqrt(stat.pixelweight);
-sparse = model.O{1}.addPrior('cauchy', 'stdvar', sqrt(2));
-slow   = model.O{1}.addPrior('slow', 'stdvar', sqrt(2));
+model.O{1}.addPrior('cauchy', 'stdvar', sqrt(2));
+model.O{1}.addPrior('slow', 'stdvar', sqrt(2));
+model.noiseStdvar = 0.3;
+%% create task
+task = GenerativeTask(taskid, taskdir, model, dataset, ...
+    'prevnet', stunit, 'iteration', istart, taskopt{:});
 %% setup inference options
 model.inferOption = struct( ...
     'Method',      'bb', ...
     'Display',     'off', ...
     'MaxIter',     40, ...
     'MaxFunEvals', 50);
-%% create prevnet
-stunit.appendto(dataset.data).aheadof(model);
-prevnet = stunit;
-%% create objectives
-lossfun = Likelihood('mse', sqrt(stat.pixelweight * whitenSizeOut * nframes) / model.noiseStdvar);
-lossfun.x.connect(model.I{1});
-lossfun.ref.connect(stunit.O{1});
-%% create task
-task = GenerativeTask(taskid, taskdir, model, dataset, lossfun, model.O{1}, ...
-    'prevnet', prevnet, 'iteration', istart);
 %% setup optmizator
 opt = HyperParam.getOptimizer();
 opt.gradmode('basic');
@@ -78,3 +73,4 @@ opt.stepmode('adapt', 'estimatedChange', initEstch);
 opt.enableRcdmode(3);
 %% run task
 task.run(nepoch, nbatch, batchsize, validsize);
+%% END
