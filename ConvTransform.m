c2e9d4041e9d27532a0e2cf5808a02eee5da53fb
MooGu Z <moogu@live.cn>
2018-07-31 21:36:24 -0400

    modify 'ConvTransform' to be able to deal with stride (working in progress)

--- ConvTransform.m
+++ ConvTransform.m
@@ -2,101 +2,29 @@ classdef ConvTransform < SISOUnit & FeedforwardOperation & Evolvable
     % NOTE: currently ConvTransform use convolution in 'SAME' shape
     methods
         function y = dataproc(obj, x)
-            % if obj.useBuildInConv2D
-            %     if obj.enableGPU
-            %         % NOTE: GPU version seems only accept symmetric
-            %         % padding, should deal with it latter.
-            %         y = nnet.internal.cnngpu.convolveForward2D( ...
-            %             x, obj.weight, ...
-            %             obj.padding.top, obj.padding.left, ...
-            %             obj.padding.bottom, obj.padding.right, ...
-            %             1, 1) + obj.bias;                    
-            %     else
-            %         y = nnet.internal.cnnhost.convolveForward2D( ...
-            %             x, obj.weight, ...
-            %             obj.padding.top, obj.padding.left, ...
-            %             obj.padding.bottom, obj.padding.right, ...
-            %             1, 1) + obj.bias;
-            %     end
-            % else
-            %    y = MathLib.nnconv(x, obj.weight, obj.bias, 'same');
-            % end
-            % z = ConvTransform.conv2d(x, obj.weight, obj.bias, obj.padding, ...
-            %     not(obj.useBuildInConv2D), obj.enableGPU, obj.useOldVersion);
-            y = ConvTransform.conv2d(x, obj.weight, obj.bias, obj.padding, ...
-                obj.useBuildInConv2D, obj.enableGPU, obj.useOldVersion);
-            % disp('[Difference of Y]');
-            % disp([y(1:5, 1:5, 1, 1), z(1:5, 1:5, 1, 1)]);
-            % fprintf('\n\n');
+            xset = ConvTransform.decomposeData(x, obj.stride);
+            y = ConvTransform.conv2d(xset, obj.WSet, obj.bias);
         end
         
         % NOTE: omit output parameter of dW and dB can save calculation
         %       power in case they are not necessary, such as, when
         %       hyperparameter is frozen.
-        function d = deltaproc(obj, d)
+        function dX = deltaproc(obj, dY)
             x = obj.I{1}.datarcd.pop();
-            % if obj.useBuildInConv2D
-            %     if obj.enableGPU
-            %         d = nnet.internal.cnngpu.convolveBackwardData2D( ...
-            %             x, obj.weight, d, ...
-            %             obj.padding.top, obj.padding.left, ...
-            %             obj.padding.bottom, obj.padding.right, ...
-            %             1, 1);
-            %         if obj.pkginfo.updateHParam
-            %             dW = nnet.internal.cnngpu.convolveBackwardFilter2D( ...
-            %                 x, obj.weight, d, ...
-            %                 obj.padding.top, obj.padding.left, ...
-            %                 obj.padding.bottom, obj.padding.right, ...
-            %                 1, 1);
-            %             dB = nnet.internal.cnngpu.convolveBackwardBias2D(d);
-            %         end
-            %     else
-            %         d = nnet.internal.cnnhost.convolveBackwardData2D( ...
-            %             x, obj.weight, d, ...
-            %             obj.padding.top, obj.padding.left, ...
-            %             obj.padding.bottom, obj.padding.right, ...
-            %             1, 1);
-            %         if obj.pkginfo.updateHParam
-            %             dW = nnet.internal.cnnhost.convolveBackwardFilter2D( ...
-            %                 x, obj.weight, d, ...
-            %                 obj.padding.top, obj.padding.left, ...
-            %                 obj.padding.bottom, obj.padding.right, ...
-            %                 1, 1);
-            %             dB = nnet.internal.cnnhost.convolveBackwardBias2D(d);
-            %         end
-            %     end
-            % else
-            %     if obj.pkginfo.updateHParam
-            %         [d, dW, dB] = MathLib.nnconvDifferential(...
-            %             d, x, obj.weight, 'same');
-            %     else
-            %         d = MathLib.nnconvDifferential(...
-            %             d, obj.I{1}.datarcd.pop(), obj.weight, 'same');
-            %     end
-            % end
+            % decompose input data into stride group
+            [xset, xpadding] = ConvTransform.decomposeData(x, obj.stride);
+            % branch of whether or not update hypter-parameters
             if obj.pkginfo.updateHParam
-                % [z, zW, zB] = ConvTransform.grad2d(d, x, obj.weight, ...
-                %     not(obj.useBuildInConv2D), obj.enableGPU, obj.useOldVersion);
-                [d, dW, dB] = ConvTransform.grad2d(d, x, obj.weight, obj.padding, ...
-                    obj.useBuildInConv2D, obj.enableGPU, obj.useOldVersion);
-            else
-                % z = ConvTransform.grad2d(d, x, obj.weight, ...
-                %     not(obj.useBuildInConv2D), obj.enableGPU, obj.useOldVersion);
-                d = ConvTransform.grad2d(d, x, obj.weight, obj.padding, ...
-                    obj.useBuildInConv2D, obj.enableGPU, obj.useOldVersion);
-            end
-            % % show difference
-            % disp('[Difference of D]');
-            % disp([d(1:5, 1:5, 1, 1), z(1:5, 1:5, 1, 1)]);
-            % disp('[Difference of dWeight]');
-            % disp([dW(:, :, 1, 1), zW(:, :, 1, 1)]);
-            % disp('[Difference of dBias]');
-            % disp([dB(:), zB(:)]);
-            % fprintf('\n\n');
-            % record gradients to Hyper-Parameters
-            if obj.pkginfo.updateHParam
+                [dXSet, dWSet, dB] = ConvTransform.grad2d(dY, xset, obj.WSet);
+                % compose gradient of data and filter
+                dX = ConvTransform.composeData(dXSet, xpadding);
+                dW = ConvTransform.composeFilter(dWSet, obj.WPadding);
+                % update hyper-parameters
                 obj.W.addgrad(dW);
                 obj.B.addgrad(dB);
+            else
+                dXSet = ConvTransform.grad2d(dY, xset, obj.WSet);
+                dX = ConvTransform.composeData(dXSet, xpadding);
             end
         end
     end
@@ -104,6 +32,11 @@ classdef ConvTransform < SISOUnit & FeedforwardOperation & Evolvable
     methods
         function hpcell = hparam(obj)
             hpcell = {obj.W, obj.B};
+        end
+        
+        function update(obj)
+            update@Evolvable(obj);
+            [obj.WSet, obj.WPadding] = ConvTransform.decomposeFilter(obj.weight, obj.stride);
         end
     end
     
@@ -122,66 +55,100 @@ classdef ConvTransform < SISOUnit & FeedforwardOperation & Evolvable
     end
     
     methods
-        function obj = ConvTransform(weight, bias)
-            obj.W = HyperParam(weight);
-            obj.B = HyperParam(reshape(bias, 1, 1, numel(bias)));
-            obj.I = {UnitAP(obj, 3, '-recdata')};
-            obj.O = {UnitAP(obj, 3)};
+        function obj = ConvTransform(weight, bias, stride)
             % calculate padding information
             obj.padding.top    = floor(size(weight, 1) / 2);
             obj.padding.bottom = floor((size(weight, 1) - 1) / 2);
             obj.padding.left   = floor(size(weight, 2) / 2);
             obj.padding.right  = floor((size(weight, 2) - 1) / 2);
+            % normalize stride
+            if not(exist('stride', 'var'))
+                obj.stride = [1, 1];
+            elseif numel(stride) == 1
+                obj.stride = stride * [1, 1];
+            end
+            % prepare filter according to stride
+            if obj.useStride
+                obj.W = obj.prepareFilter(weight, stride);
+            else
+                obj.W = HyperParam(weight); 
+            end
+            obj.B = HyperParam(reshape(bias, 1, 1, numel(bias)));
+            % initialize IO
+            obj.I = {UnitAP(obj, 3, '-recdata')};
+            obj.O = {UnitAP(obj, 3)};
         end
     end
     
     methods (Static)
-        function y = conv2d(x, weight, bias, padding, fBuildIn, fGPU, fOldVer)
-            if fBuildIn % use build-in function from neural-network toolbox
-                issymmetric = (padding.top == padding.bottom) && (padding.left == padding.right);
-                % padding array for non-symmetric cases (or GPU)
-                if not(issymmetric)
-                    if fGPU || (not(fGPU) && fOldVer)
-                        x = padarray(x, [padding.top, padding.left] - 1, 0, 'pre');
-                        x = padarray(x, [padding.bottom, padding.right] + 1, 0, 'post');
-                        padding = struct('top', 0, 'left', 0, 'bottom', 0, 'right', 0);
+        function [xset, xpadding] = decomposeData(x, stride)
+            xset = cell([stride, size(x, 3)]);
+            [xset(:, :, 1), xpadding] = getStrideSet(x(:, :, 1), stride);
+            for i = 2 : size(x, 3)
+                xset(:, :, i) = getStrideSet(x(:, :, i), stride);
+            end
+        end
+        
+        function x = composeData(xset, xpadding)
+            [h, w]    = size(xset{1});
+            [m, n, c] = size(xset);
+            % combine matrix in cell array
+            x = cat(3, xset{:});
+            x = reshape(x, [h, w, m, n, c]);
+            x = permute(x, [3, 1, 4, 2, 5]);
+            x = reshape(x, [h * m, w * n, c]);
+            % remove zero-padding
+            x = x(1 : end - xpadding(1), 1 : end - xpadding(2), :);
+        end
+        
+        function [wset, wpadding] = decomposeFilter(w, stride)
+            wset = cell([stride, size(w, 3), size(w, 4)]);
+            referPoint = ceil(([size(w, 1), size(w, 2)] + 1) / 2);
+            for i = 1 : size(w, 3)
+                for j = 1 : size(w, 4)
+                    if exist('wpadding', 'var')
+                        wset(:, :, i, j) = getStrideSet(w(:, :, i, j), stride, referPoint, 'reverse');
                     else
-                        padding.top    = padding.top    - 1;
-                        padding.left   = padding.left   - 1;
-                        padding.bottom = padding.bottom + 1;
-                        padding.right  = padding.right  + 1;
-                    end
-%                     if fGPU || (not(fGPU) && fOldVer)
-%                         x = padarray(x, [padding.top, padding.left], 0, 'pre');
-%                         x = padarray(x, [padding.bottom, padding.right] + 1, 0, 'post');
-%                         padding = struct('top', 0, 'left', 0, 'bottom', 0, 'right', 0);
-%                     end
-                end
-                % flip x-axis and y-axis to actually do convlution not correlation
-                weight = matflip(weight);
-                % do convolution under GPU or CPU environment
-                if fGPU
-                    if fOldVer || ispc
-                        y = nnet.internal.cnngpu.convolveForward2D( ...
-                            x, weight, padding.top, padding.left, 1, 1) + bias;
-                    else
-                        y = nnet.internal.cnngpu.convolveForward2D( ...
-                            x, weight, padding.top, padding.left, ...
-                            padding.bottom, padding.right, 1, 1) + bias;
-                    end
-                else
-                    if fOldVer
-                        y = nnet.internal.cnnhost.convolveForward2D( ...
-                            x, weight, padding.top, padding.left, 1, 1) + bias;
-                    else
-                        y = nnet.internal.cnnhost.convolveForward2D( ...
-                            x, weight, padding.top, padding.left, ...
-                            padding.bottom, padding.right, 1, 1) + bias;
+                        [wset(:, :, i, j), wpadding] = ...
+                            getStrideSet(w(:, :, i, j), stride, referPoint, 'reverse');
                     end
                 end
-            else
-                y = MathLib.nnconv(x, weight, bias, 'same');
-            end        
+            end
+        end
+        
+        function weight = composeFilter(wset, wpadding)
+            [h, w]       = size(wset{1});
+            [m, n, c, f] = size(wset);
+            % flip cell array in both 1st and 2nd dimension
+            wset = flip(flip(wset, 1), 2);
+            % combine matrix in cell array
+            weight = cat(3, wset{:});
+            weight = reshape(weight, [h, w, m, n, c, f]);
+            weight = permute(weight, [3, 1, 4, 2, 5, 6]);
+            weight = reshape(weight, [h * m, w * n, c, f]);
+            % remove zero-padding
+            weight = weight( ...
+                wpadding(1) + 1 : end - wpadding(3), wpadding(2) + 1 : end - wpadding(4), :, :);
+        end
+    end
+    
+    methods (Static)
+        function y = conv2d(x, f, bias)
+            y = cell(size(f, 4), 1);
+            for n = 1 : numel(y)
+                y{n} = 0;
+                for i = 1 : size(x, 1)
+                    for j = 1 : size(x, 2)
+                        for k = 1 : size(x, 3)
+                            y{n} = y{n} + conv2(x{i, j, k}, f{i, j, k, n}, 'same');
+                        end
+                    end
+                end
+            end
+            % combine cells into a matrix
+            y = cat(3, y{:});
+            % add bias
+            y = bsxfun(@plus, y, reshape(bias, [1, 1, numel(bias)]));
         end
         
         function [d, dW, dB] = grad2d(d, x, weight, padding, fBuildIn, fGPU, fOldVer)
@@ -293,6 +260,10 @@ classdef ConvTransform < SISOUnit & FeedforwardOperation & Evolvable
         taxis = false;
     end
     
+    properties
+        stride
+    end
+    
     properties (Access = protected)
         W, B, padding
     end
@@ -306,6 +277,7 @@ classdef ConvTransform < SISOUnit & FeedforwardOperation & Evolvable
         end
         function set.weight(obj, value)
             obj.W.set(value);
+            [obj.WSet, obj.WPadding] = ConvTransform.decomposeFilter(value, obj.stride);
         end
         
         function value = get.bias(obj)
